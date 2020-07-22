baa1438f8ccfa7b5d9ab2617b5da4c329b95fa2b
Luke Esworthy <lukesworthy@gmail.com>
2020-07-22 15:59:28 -0500

    added code for mountain class

--- environments/mountain.py
+++ environments/mountain.py
@@ -1,4 +1,30 @@
-class Mountain():
+from animals import Identifiable
+from .contains_animals import ContainsAnimals
+from .contains_plants import ContainsPlants
+
+
+class Mountain(ContainsAnimals, ContainsPlants, Identifiable):
 
     def __init__(self):
-        pass
+        ContainsAnimals.__init__(self)
+        ContainsPlants.__init__(self)
+        Identifiable.__init__(self)
+        self.plant_capacity = 4
+        self.animal_capacity = 6
+        self.characteristics = ["high elevation"]
+
+    def add_animal(self, animal):
+        try:
+            if animal.terrestrial:
+                self.animals.append(animal)
+        except AttributeError:
+            raise AttributeError(
+                "Cannot add aquatic animals to a mountain")
+
+    def add_plant(self, plant):
+        try:
+            if plant.location == "Mountain":
+                self.plants.append(plant)
+        except AttributeError:
+            raise AttributeError(
+                "Cannot add non-mountainous plants to a mountain biome")
