bbb543fe0e1269ba827584bc7c20d26c3d477d96
Luke Esworthy <lukesworthy@gmail.com>
2020-07-23 15:11:03 -0500

    finalized mountain class

--- environments/mountain.py
+++ environments/mountain.py
@@ -9,19 +9,22 @@ class Mountain(Identifiable, ContainsAnimals, ContainsPlants):
         Identifiable.__init__(self)
         ContainsAnimals.__init__(self)
         ContainsPlants.__init__(self)
+        self.plant_capacity = 4
+        self.animal_capacity = 6
+        self.characteristics = ["high elevation"]
 
     def add_animal(self, animal):
         try:
-            if animal.flying and animal.terrestrial and animal.mammal:
+            if animal.mammal and animal.flying and animal.terrestrial:
                 self.animals.append(animal)
         except AttributeError:
             raise AttributeError(
-                "Cannot add non-mammal or aquatic animals to a mountain environment.")
+                "Cannot add aquatic animals to a mountain environment.")
 
     def add_plant(self, plant):
         try:
-            if plant.location == "mountain":
+            if plant.sunlight.lower() == "partial":
                 self.plants.append(plant)
         except AttributeError:
             raise AttributeError(
-                "Cannot add non-mountainous plants to a mountain environment.")
+                "Mountain flora require partial sunlight.")
