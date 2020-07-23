8ca6410798a838365d12acc582e25e87f2989e10
Luke Esworthy <lukesworthy@gmail.com>
2020-07-23 14:24:28 -0500

    partially complete mountain biome class

--- environments/mountain.py
+++ environments/mountain.py
@@ -2,6 +2,7 @@ from animals import Identifiable
 from .contains_animals import ContainsAnimals
 from .contains_plants import ContainsPlants
 
+
 class Mountain(Identifiable, ContainsAnimals, ContainsPlants):
 
     def __init__(self):
@@ -11,4 +12,16 @@ class Mountain(Identifiable, ContainsAnimals, ContainsPlants):
 
     def add_animal(self, animal):
         try:
-            if animal.
+            if animal.flying and animal.terrestrial and animal.mammal:
+                self.animals.append(animal)
+        except AttributeError:
+            raise AttributeError(
+                "Cannot add non-mammal or aquatic animals to a mountain environment.")
+
+    def add_plant(self, plant):
+        try:
+            if plant.location == "mountain":
+                self.plants.append(plant)
+        except AttributeError:
+            raise AttributeError(
+                "Cannot add non-mountainous plants to a mountain environment.")
