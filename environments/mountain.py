b829d27d5dff46dc158fd2995ecb46ab4ac7281f
Luke Esworthy <lukesworthy@gmail.com>
2020-07-23 13:30:19 -0500

    started mountain class, commit before changing branch

--- environments/mountain.py
+++ environments/mountain.py
@@ -1,4 +1,14 @@
-class Mountain():
+from animals import Identifiable
+from .contains_animals import ContainsAnimals
+from .contains_plants import ContainsPlants
+
+class Mountain(Identifiable, ContainsAnimals, ContainsPlants):
 
     def __init__(self):
-        pass
+        Identifiable.__init__(self)
+        ContainsAnimals.__init__(self)
+        ContainsPlants.__init__(self)
+
+    def add_animal(self, animal):
+        try:
+            if animal.
