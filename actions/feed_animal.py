f7969e815a002f073f10ed5315317d9aeb0e3b2d
Luke Esworthy <lukesworthy@gmail.com>
2020-07-24 11:51:59 -0500

    commit with menu created, getting ready to pull new master

--- actions/feed_animal.py
+++ actions/feed_animal.py
@@ -1,12 +1,22 @@
 
 
 def feed_animal(arboretum):
-    
-    for animal in arboretum.name:
-        print(f"{index + 1}. {animal}")
-    else:
-        print("Choose animal to feed.")
 
+    print("-------------------------------")
+    print("1. Pueo")
+    print("2. River Dolphin")
+    print("3. \'Ulae")
+    print("4. Gold Dust Day Gecko")
+    print("5. Nene Goose")
+    print("6. Kīkākapu")
+    print("7. Ope\'ape\'a")
+    print("8. Hawaiian Happy-Face Spider")
+    print("")
+    print("Choose animal to feed.")
     choice = input("> ")
 
+    if choice == "1":
+        
 
+    # for index, animal in enumerate(arboretum.animals):
+    #     print(f"{index + 1}. {animal}")
