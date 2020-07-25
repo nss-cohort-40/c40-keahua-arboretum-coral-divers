7fd7f2374765ac4a6540e5cb2459e3635f74ec29
David Everett <davideverett1989@gmail.com>
2020-07-24 21:02:10 -0500

    It ain't pretty, but it works.

--- actions/release_animal.py
+++ actions/release_animal.py
@@ -28,23 +28,28 @@ def release_animal(arboretum):
     if choice == "1":
         animal = GoldDustDayGecko()
     if choice == "2":
-        animal == RiverDolphin()
+        animal = RiverDolphin()
     if choice == "3":
-        animal == NeneGoose()
+        animal = NeneGoose()
     if choice == "4":
-        animal == Kikakapu()
+        animal = Kikakapu()
     if choice == "5":
-        animal == Pueo()
+        animal = Pueo()
     if choice == "6":
-        animal == Ulae()
+        animal = Ulae()
     if choice == "7":
-        animal == Opeapea()
+        animal = Opeapea()
     if choice == "8":
-        animal == HappyFacedSpider()
-
-    for key, value in arboretum.__dict__.items():
+        animal = HappyFacedSpider()
+    # Changing the Arboretum to a dictionary and looping through it.
+    for (key, value) in arboretum.__dict__.items():
+        # When the loop finds a list...
         if type(value) is list:
-            for biome in value:
+            # ...then loop through that list and reference each index.
+            for index, biome in enumerate(value):
+                # Smash the index number into a new property on the object at that list index...
+                biome.original_index = index
+                # ...so that when it is appended into a new list it can be referenced later.
                 allBiomes.append(biome)
                 
     for index, biome in enumerate(allBiomes):
@@ -52,7 +57,18 @@ def release_animal(arboretum):
 
     print("Release the animal into which biome?")
     choice = input("> ")
-
-    arboretum.rivers[int(choice) - 1].animals.append(animal)
-
-
+    index = int(choice) - 1
+    selected_biome = allBiomes[index]
+    original_biome_index = selected_biome.original_index
+    if selected_biome.name == "Coastline":
+        arboretum.coastlines[original_biome_index].animals.append(animal)
+    if selected_biome.name == "Forest":
+        arboretum.forests[original_biome_index].animals.append(animal)
+    if selected_biome.name == "Grassland":
+        arboretum.grasslands[original_biome_index].animals.append(animal)
+    if selected_biome.name == "Mountain":
+        arboretum.mountains[original_biome_index].animals.append(animal)
+    if selected_biome.name == "River":
+        arboretum.rivers[original_biome_index].animals.append(animal)
+    if selected_biome.name == "Swamp":
+        arboretum.swamps[original_biome_index].animals.append(animal)
