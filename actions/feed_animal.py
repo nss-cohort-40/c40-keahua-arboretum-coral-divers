761a3169149b3ec2928ddaf6b47e9ae534157f8a
Luke Esworthy <lukesworthy@gmail.com>
2020-07-27 10:01:36 -0500

    commit with initial steps created

--- actions/feed_animal.py
+++ actions/feed_animal.py
@@ -22,18 +22,94 @@ def feed_animal(arboretum):
     print("5. Nene Goose")
     print("6. Kīkākapu")
     print("7. Ope\'ape\'a")
-    print("8. Hawaiian Happy-Face Spider")
+    print("8. Hawaiian Happy-Faced Spider")
     print("")
     print("Choose animal to feed.")
     choice = input("> ")
 
-    if choice == "1":
+    animal_collection = []
+
+    if choice == "2":
+        for river in arboretum.rivers:
+            for animal in river.animals:
+                print(f"      {animal.species}  {animal.id}")
+        for swamp in arboretum.swamps:
+            for animal in swamp.animals:
+                print(f"      {animal.species}  {animal.id}")
+        for coastline in arboretum.coastlines:
+            for animal in coastline.animals:
+                print(f"      {animal.species}  {animal.id}")
         for grassland in arboretum.grasslands:
             for animal in grassland.animals:
-                if animal.species.lower() == "pueo":
-                    print(f"{animal.species}")
-                    input("\n\nPress any key to continue...")
+                print(f"      {animal.species}  {animal.id}")
+        for forest in arboretum.forests:
+            for animal in forest.animals:
+                print(f"      {animal.species}  {animal.id}")
+        for mountain in arboretum.mountains:
+            for animal in mountain.animals:
+                print(f"      {animal.species}  {animal.id}")
+        input("Press ENTER to continue")
 
+    # for river in arboretum.rivers:
+    #     for animal in river.animals:
+    #         animal_collection.append(animal)
+    # for swamp in arboretum.swamps:
+    #     for animal in swamp.animals:
+    #         animal_collection.append(animal)
+    # for coastline in arboretum.coastlines:
+    #     for animal in coastline.animals:
+    #         animal_collection.append(animal)
+    # for grassland in arboretum.grasslands:
+    #     for animal in grassland.animals:
+    #         animal_collection.append(animal)
+    # for forest in arboretum.forests:
+    #     for animal in forest.animals:
+    #         animal_collection.append(animal)
+    # for mountain in arboretum.mountains:
+    #     for animal in mountain.animals:
+    #         animal_collection.append(animal)
+
+    # if choice == "1":
+    #     for animal in animal_collection:
+    #         if animal.species.lower() == "pueo":
+    #             print(f"{animal.species} {animal.id}")
+    # if choice == "2":
+    #     for animal in animal_collection:
+    #         if animal.species.lower() == "river dolphin":
+    #             print(f"{animal.species} {animal.id}")
+    # if choice == "3":
+    #     for animal in animal_collection:
+    #         if animal.species.lower() == "ulae":
+    #             print(f"{animal.species} {animal.id}")
+    # if choice == "4":
+    #     for animal in animal_collection:
+    #         if animal.species.lower() == "gold dust day gecko":
+    #             print(f"{animal.species} {animal.id}")
+    # if choice == "5":
+    #     for animal in animal_collection:
+    #         if animal.species.lower() == "nene goose":
+    #             print(f"{animal.species} {animal.id}")
+    # if choice == "6":
+    #     for animal in animal_collection:
+    #         if animal.species.lower() == "kikakapu":
+    #             print(f"{animal.species} {animal.id}")
+    # if choice == "7":
+    #     for animal in animal_collection:
+    #         if animal.species.lower() == "opeapea":
+    #             print(f"{animal.species} {animal.id}")
+    # if choice == "8":
+    #     for animal in animal_collection:
+    #         if animal.species.lower() == "hawaiian happy-faced spider":
+    #             print(f"{animal.species} {animal.id}")
+
+
+# More bad code to reference:
+    # if choice == "1":
+    #     for grassland in arboretum.grasslands:
+    #         for animal in grassland.animals:
+    #             if animal.species.lower() == "pueo":
+    #                 print(f"{animal.species}")
+    #                 input("\n\nPress any key to continue...")
 
 
 # Previous code to reference:
