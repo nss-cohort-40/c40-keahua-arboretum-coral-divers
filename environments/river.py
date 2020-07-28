1c055aa51095e53ea0c61a2a1a5b9f4c6f357830
Daniel Meza <daniel.meza29@yahoo.com>
2020-07-28 13:40:55 -0500

    resolved river conflicts

--- environments/river.py
+++ environments/river.py
@@ -1,25 +1,13 @@
 from .biome import Biome
 
+
 class River(Biome):
 
     def __init__(self):
-        Biome.__init__(self, "River", 6, 12, "Fresh Water")
+        Biome.__init__(self, "River", 6, 1, "Fresh Water")
 
     def add_animal(self, animal):
         try:
-<<<<<<< HEAD
-            if hasattr(animal, 'is_euryhaline'):
-                self.animals.append(animal)
-                print(f'{animal.species} was added to the river!')
-                input('Please press enter to continue...')
-            elif animal.aquatic and animal.cell_type == "hypertonic":
-                self.animals.append(animal)
-                print(f'{animal.species} was added to the river!')
-                input('Please press enter to continue...')
-        except AttributeError:
-            print("Cannot add non-aquatic, or saltwater animals to a river")
-
-=======
             if len(self.animals) < self.animal_capacity:
                 if hasattr(animal, 'is_euryhaline'):
                     self.animals.append(animal)
@@ -30,14 +18,14 @@ class River(Biome):
                     print(f'{animal.species} was added to the river!')
                     input('Please press enter to continue...')
                 else:
-                    raise AttributeError('This animal does not belong in the river!')
+                    raise AttributeError(
+                        'This animal does not belong in the river!')
             else:
                 animal_accepted = False
                 return animal_accepted
                 raise AttributeError('The river has already reached capacity!')
         except AttributeError as err:
             print(err)
->>>>>>> master
             input('Press enter to continue')
 
     def add_plant(self, plant):
